eb1367235042113b9226c40a3ae338c03c22a109
iPanja <fhenneman@comcast.net>
2024-03-14 17:14:36 -0400

    fixed resume preview

--- pages/resume.jsx
+++ pages/resume.jsx
@@ -9,7 +9,7 @@ function Resume() {
       <div className="mb-4">
         { isLoading ? <p>Loading... 🔎</p> : <p></p> }
       </div>
-      <iframe src="https://docs.google.com/document/d/18aYfXMHnfElo8lb8jshAvyPup1JgV1hDqaUtL-iJVSk/preview" width="640" height="850" allow="autoplay" onLoad={() => setIsLoading(false)} />
+      <iframe src="https://docs.google.com/file/d/18aYfXMHnfElo8lb8jshAvyPup1JgV1hDqaUtL-iJVSk/preview" width="640" height="850" allow="autoplay" onLoad={() => setIsLoading(false)} />
     </>
   );
 }

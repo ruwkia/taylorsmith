eca80f220a33022a0302940f76d714e7624823bf
ruwkia <144492896+ruwkia@users.noreply.github.com>
2025-01-11 17:17:36 -0500

    Update about.jsx

--- pages/about.jsx
+++ pages/about.jsx
@@ -48,14 +48,14 @@ function About() {
       <ul className="list-disc list-inside">
         <li>
           Worked with a team to create an immersive VR experience, 
-           <a href="https://youtu.be/bJZfQfwQ78Q?si=JaYS-eD3R8cW2XYY" target="_blank" rel="noopener noreferrer">Nautilus</a>
+           <a href="https://youtu.be/bJZfQfwQ78Q?si=JaYS-eD3R8cW2XYY" target="_blank" rel="noopener noreferrer"> Nautilus</a>
         </li>
         <li>
           Interned at Penn State Sustainability for a year, focusing on graphic design and social media management
           </li>
         <li>
           Collaborated with my best friend on a game, 
-           <a href="https://cndles.itch.io/space-control" target="_blank" rel="noopener noreferrer">Space Control</a>, available on itch.io
+           <a href="https://cndles.itch.io/space-control" target="_blank" rel="noopener noreferrer"> Space Control</a>, available on itch.io
           </li>
       </ul>
       <p>

b695bcf4263d73973a465802792961c6e6ceeb55
tay <144492896+ruwkia@users.noreply.github.com>
2025-07-22 15:12:17 -0400

    Update spacecontrol.jsx

--- pages/spacecontrol.jsx
+++ pages/spacecontrol.jsx
@@ -39,16 +39,15 @@ function SpaceControl() {
   flexDirection: 'column',
   gap: '2rem',
   marginBottom: '2rem',
-  width: '100vw',
-  marginLeft: 'calc(-50vw + 50%)'
+  alignItems: 'center'
 }}>
-  <img src="/images/artworks/spacecontrolhope.png" alt="Hope screen" style={{ width: '100%' }} />
-  <img src="/images/artworks/spacecontrolleave.png" alt="Leave screen" style={{ width: '100%' }} />
-  <img src="/images/artworks/spacecontroltopsecret.png" alt="Top Secret screen" style={{ width: '100%' }} />
-  <img src="/images/artworks/spacecontrolsus.png" alt="Suspicious screen" style={{ width: '100%' }} />
-  <img src="/images/artworks/spacecontrolpuzzle.png" alt="Puzzle screen" style={{ width: '100%' }} />
-  <img src="/images/artworks/spacecontrolslider.png" alt="Slider interface" style={{ width: '100%' }} />
-  <img src="/images/artworks/spacecontrolemail.png" alt="Email screen" style={{ width: '100%' }} />
+  <img src="/images/artworks/spacecontrolhope.png" alt="Hope screen" style={{ width: '100%', maxWidth: '1000px' }} />
+  <img src="/images/artworks/spacecontrolleave.png" alt="Leave screen" style={{ width: '100%', maxWidth: '1000px' }} />
+  <img src="/images/artworks/spacecontroltopsecret.png" alt="Top Secret screen" style={{ width: '100%', maxWidth: '1000px' }} />
+  <img src="/images/artworks/spacecontrolsus.png" alt="Suspicious screen" style={{ width: '100%', maxWidth: '1000px' }} />
+  <img src="/images/artworks/spacecontrolpuzzle.png" alt="Puzzle screen" style={{ width: '100%', maxWidth: '1000px' }} />
+  <img src="/images/artworks/spacecontrolslider.png" alt="Slider interface" style={{ width: '100%', maxWidth: '1000px' }} />
+  <img src="/images/artworks/spacecontrolemail.png" alt="Email screen" style={{ width: '100%', maxWidth: '1000px' }} />
 </div>
 
 
